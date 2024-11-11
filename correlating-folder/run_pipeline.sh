f5995a7f928ca01cb612ac860a5e3d6ae0f20ceb
danielbilsborrow6 <danielbilsborrow@gmail.com>
2024-11-11 11:19:46 +0000

    updating corrs

--- correlating-folder/run_pipeline.sh
+++ correlating-folder/run_pipeline.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+mkdir -p CAMELS_CONFIGS
+
 # Loop through desired range to create multiple config files and execute the pipeline
-for i in {0..29}; do
+for i in {0..30}; do
     # Replace placeholder in config file
-    sed "s/\${MY_VARIABLE}/$i/g" CAMELS_config.ini > CAMELS_config$i.ini
+    sed "s/\${MY_VARIABLE}/$i/g" CAMELS_config.ini > CAMELS_CONFIGS/CAMELS_config$i.ini
     
     # Execute the pipeline with the new config file
-    python w_pipeline1.py CAMELS_config$i.ini
+    python w_pipeline1.py CAMELS_CONFIGS/CAMELS_config$i.ini
     
     echo $i
 done
