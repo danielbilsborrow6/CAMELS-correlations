aa5a2eefc4404263f53d014cce1223a1d080756c
danielbilsborrow6 <danielbilsborrow@gmail.com>
2024-11-12 13:43:12 +0000

    consistent correlations

--- correlating-folder/run_pipeline.sh
+++ correlating-folder/run_pipeline.sh
@@ -2,15 +2,13 @@
 
 mkdir -p CAMELS_CONFIGS
 
-# Loop through desired range to create multiple config files and execute the pipeline
-for i in {0..30}; do
+# Generate a list of numbers and pass them to xargs to execute in parallel
+seq 0 1000 | xargs -n 1 -P 11 -I {} bash -c '
     # Replace placeholder in config file
-    sed "s/\${MY_VARIABLE}/$i/g" CAMELS_config.ini > CAMELS_CONFIGS/CAMELS_config$i.ini
+    sed "s/\${MY_VARIABLE}/$1/g" CAMELS_config.ini > CAMELS_CONFIGS/CAMELS_config$1.ini
     
     # Execute the pipeline with the new config file
-    python w_pipeline1.py CAMELS_CONFIGS/CAMELS_config$i.ini
+    python w_pipeline1.py CAMELS_CONFIGS/CAMELS_config$1.ini
     
-    echo $i
-done
-
-
+    echo "$1"
+' _ {}
